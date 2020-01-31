b9d1082f2dfd233ff4bf9f61c543dc6401053792
HexSeal <maxfin13@gmail.com>
2020-01-30 16:15:59 -0800

    [fix] readFile works, figuring out writefile

--- makesite.go
+++ makesite.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
+
+func readFile(file string) {
+        fileContents, err := ioutil.ReadFile(file)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Print(string(fileContents))
+}
+
+func writeFile() {
+	contents := readFile("first-post.txt")
+	bytesToWrite := []byte(contents)
+
+	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	readFile("first-post.txt")
+	writeFile()
 }
